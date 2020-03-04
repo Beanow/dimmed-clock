1eb5c43c79ce365d6ad0f56cce1988cb9e828a3c
Robin <robin@robin-it.com>
2020-03-04 16:27:36 +0100

    Set up github action

--- src/storage.js
+++ src/storage.js
@@ -1,12 +1,14 @@
 const LOCAL_STORAGE_KEY = "beanow:dimmed-clock:config";
 
 export const set = (state) => {
+	if (typeof window === "undefined") return;
 	if (!window.localStorage) return;
 	window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
 };
 
 export const get = () => {
-	if (!window.localStorage) return;
+	if (typeof window === "undefined") return {};
+	if (!window.localStorage) return {};
 	const json = window.localStorage.getItem(LOCAL_STORAGE_KEY);
 	if (!json) return {};
 	return JSON.parse(json);
